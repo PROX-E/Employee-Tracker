621e3be8df8ef6af82e6791f3f19db0f2e4594b9
PROX-E <lichlord18@gmail.com>
2020-03-18 11:29:32 -0500

    getManagerNames Function
    creates query for null manager ids,
    creates name array for input,
    pushes the (formatted) names to the array, then returns formatted names

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ const db = new Database({
     database: "cms"
   });
   
+async function getManagerNames() {
+    let query = "SELECT * FROM employee WHERE manager_id IS NULL";
+
+    const rows = await db.query(query);
+    //console.log("number of rows returned " + rows.length);
+    let employeeNames = [];
+    for(const employee of rows) {
+        employeeNames.push(employee.first_name + " " + employee.last_name);
+    }
+    return employeeNames;
+}

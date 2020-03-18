46863ae946e21beceba872669b15d36227950207
PROX-E <lichlord18@gmail.com>
2020-03-18 11:21:36 -0500

    create query to db data

--- async-db.js
+++ async-db.js
@@ -4,6 +4,21 @@ class Database {
     constructor( config ) {
         this.connection = mysql.createConnection( config );
     }
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err ) {
+                    console.log(err.sql);
+                    console.log("");
+                    return reject( err );
+                }
+                resolve( rows );
+            } );
+        } );
+    }
 }
 
 
